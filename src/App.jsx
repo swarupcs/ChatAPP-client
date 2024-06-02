51fc6c1f604c136847503cd51bec014b1eda35eb
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-06-02 15:20:14 +0530

    Signin Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/chat/:chatId' element={<Chat/>}/>
+        <Route path='/groups' element={<Groups/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
---
+++ src/pages/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  
+  const toggleLogin = () => setIsLogin(false);
+
+  return (
+    <Container component={"main"} maxWidth="xs" sx={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      }}>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? (
+          <>
+            <Typography>Login</Typography>
+            <form 
+            style={{width: "100%", marginTop: "1rem"}}>
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+              sx={{margnTop: "1rem"}}
+              variant="contained"
+              color="primary"
+              type="submit"
+              fullWidth
+              >
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+
+              <Button
+              fullWidth
+              variant="text"
+              onClick={toggleLogin}
+              >
+                Sign Up Instead
+              </Button>
+               
+            </form>
+          </>
+        ) : (
+          <span>register</span>
+        )}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
+
+
