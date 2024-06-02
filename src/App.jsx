bc85f04e17cf859f0c131e29edaa712e82708b0a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-06-02 19:05:34 +0530

    Protected Route Setup for /login and /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import React, { lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 
+let user = true;
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/chat/:chatId' element={<Chat/>}/>
-        <Route path='/groups' element={<Groups/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
+        <Route path="/login" element={
+        <ProtectRoute user={!user} redirect="/">
+          <Login/>
+        </ProtectRoute>
+        } />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 
+/**
+ * Protected Routes => If user is logged in, he cann't go for login , but if the user is not login, so he don't go for home page
+ * 
+*/
