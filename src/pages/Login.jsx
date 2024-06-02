924ca4e0b2cbd92ca9b0166c5a302b97348dd4db
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-06-02 15:26:40 +0530

    SignUp Part Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Button, Container, Paper, TextField, Typography } from "@mui/material";
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
   
-  const toggleLogin = () => setIsLogin(false);
+  const toggleLogin = () => setIsLogin((prev) => !prev);
 
   return (
     <Container component={"main"} maxWidth="xs" sx={{
@@ -65,7 +65,62 @@ const Login = () => {
             </form>
           </>
         ) : (
-          <span>register</span>
+          <>
+          <Typography>Sign Up</Typography>
+          <form 
+          style={{width: "100%", marginTop: "1rem"}}>
+                        <TextField
+              required
+              fullWidth
+              label="Name"
+              margin="normal"
+              variant="outlined"
+            />
+
+<TextField
+              required
+              fullWidth
+              label="Bio"
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Username"
+              margin="normal"
+              variant="outlined"
+            />
+
+            <TextField
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              margin="normal"
+              variant="outlined"
+            />
+            <Button
+            sx={{margnTop: "1rem"}}
+            variant="contained"
+            color="primary"
+            type="submit"
+            fullWidth
+            >
+              Sign Up
+            </Button>
+            <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+
+            <Button
+            fullWidth
+            variant="text"
+            onClick={toggleLogin}
+            >
+              Login Instead
+            </Button>
+             
+          </form>
+        </>
         )}
       </Paper>
     </Container>
